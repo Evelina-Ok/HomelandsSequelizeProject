2659f96e748f4a8994933e5225c7c29be9e583f7
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2025-02-28 09:13:15 +0100

    #fixed error in gitignore

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,4 +4,4 @@ import { Authenticate, Authorize } from '../utils/authUtils.js';
 export const authController = express.Router()
 
 authController.post('/login', (req, res) => { Authenticate(req, res) })
-authController.get('/authorize', Authorize, (req, res, next) => { res.send({ message: 'You are logged in'}) })
+authController.get('/authorize', Authorize, (req, res, next) => { res.send({ message: 'You are logged in'}) }) 
